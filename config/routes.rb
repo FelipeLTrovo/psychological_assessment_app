9fd60bee472d39441d1c0c942a6aa078e0c85eee
FelipeLTrovo <felipeltrovo@hotmail.com>
2024-04-12 16:54:47 -0300

    Adds routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  resources :patients
+  resources :questions
+  resources :instruments
+  resources :instruments_applier, only: %i[new create]
+  resources :instruments_solver, only: %i[index new create]
+  devise_for :psychologists, controllers: { registrations: 'psychologists/registrations'  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,5 +12,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "home#index"
 end
