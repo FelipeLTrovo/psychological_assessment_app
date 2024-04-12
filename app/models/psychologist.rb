18a4ec19350a054ca04d650f73ffe6da17a3a537
FelipeLTrovo <felipeltrovo@hotmail.com>
2024-04-12 16:53:51 -0300

    Renames user devise model to psychologist.

--- app/models/psychologist.rb
+++ app/models/psychologist.rb
@@ -1,6 +1,10 @@
-class User < ApplicationRecord
+class Psychologist < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Relationships
+  has_one :psychologist
+  has_one :patient
 end
