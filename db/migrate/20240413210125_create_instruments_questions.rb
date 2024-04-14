0790f6763eb1542898fc8cbf5f66741d136858ad
FelipeLTrovo <felipeltrovo@hotmail.com>
2024-04-14 14:59:25 -0300

    Adds InstrumentsQuestions table.

---
+++ db/migrate/20240413210125_create_instruments_questions.rb
@@ -0,0 +1,10 @@
+class CreateInstrumentsQuestions < ActiveRecord::Migration[7.1]
+  def change
+    create_table :instruments_questions do |t|
+      t.references :instrument, null: false, foreign_key: true
+      t.references :question, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
